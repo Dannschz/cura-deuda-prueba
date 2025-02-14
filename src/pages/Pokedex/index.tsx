d0b073af50b485856dd9524a894e7de370026ed7
Dannschz <daniel.snchz.develop@outlook.com>
2025-02-14 12:38:47 -0600

    love changesss

--- src/pages/Pokedex/index.tsx
+++ src/pages/Pokedex/index.tsx
@@ -1,17 +1,17 @@
 import HeaderSearchAndMenu from '../../components/HeaderSearchAndMenu'
 import PokemonList from '../../components/PokemonList'
-import useSearchHook from '../../hooks/useSearchHook'
+// import useSearchHook from '../../hooks/useSearchHook'
 import { useAppSelector } from '../../redux/hooks'
 import './styles.scss'
 
 // Contiene la lista de los pokemones en la pokedex
 function Pokedex() {
-  const setSearchValue = useSearchHook()
+  // const setSearchValue = useSearchHook()
   const store = useAppSelector(state => state) // el store global
 
   return (
     <div className='pokedexPage'>
-      <HeaderSearchAndMenu setSearchValue={setSearchValue} />
+      <HeaderSearchAndMenu />
       <h2 className='pp-title'>My Pokedex</h2>
       {store.pokemons.value.length > 0 ? (
         <PokemonList
--- src/pages/PokemonDetail/index.tsx
+++ src/pages/PokemonDetail/index.tsx
@@ -1,11 +1,11 @@
-import React, { useEffect } from 'react'
+/* import React, { useEffect } from 'react'
 import HeaderSearchAndMenu from '../../components/HeaderSearchAndMenu'
 import PokemonAbilities from '../../components/PokemonAbilities'
 import PokemonDetailLeft from '../../components/PokemonDetailLeft'
 import PokemonFeatures from '../../components/PokemonFeatures'
 import PokemonStats from '../../components/PokemonStats'
-import { Pokemon } from '../../components/types'
-import useSearchHook from '../../hooks/useSearchHook'
+import { Pokemon } from '../../components/types' */
+// import useSearchHook from '../../hooks/useSearchHook'
 import './styles.scss'
 
 interface PokemonDetailsProps {
@@ -14,10 +14,10 @@ interface PokemonDetailsProps {
 
 // Contiene la información del pokemon
 function PokemonDetail({ params }: PokemonDetailsProps) {
-  const setSearchValue = useSearchHook()
-  const [pokemonDetail, setPokemonDetail] = React.useState<Pokemon>()
+  // const setSearchValue = useSearchHook()
+  // const [pokemonDetail, setPokemonDetail] = React.useState<Pokemon>()
 
-  useEffect(() => {
+  /* useEffect(() => {
     const getPokemon = async () => {
       const response = await fetch(
         `https://pokeapi.co/api/v2/pokemon/${params.name}`
@@ -27,22 +27,11 @@ function PokemonDetail({ params }: PokemonDetailsProps) {
     }
     getPokemon()
     console.log(pokemonDetail)
-  }, [params.name])
+  }, [params.name]) */
 
   return (
     <>
-      <HeaderSearchAndMenu setSearchValue={setSearchValue} />
-      <div className='pokemonDetailContainer'>
-        <PokemonDetailLeft pokemon={pokemonDetail} />
-        <PokemonAbilities abilities={pokemonDetail?.abilities ?? []} />
-        <PokemonStats stats={pokemonDetail?.stats ?? []} />
-        <PokemonFeatures
-          features={{
-            moves: pokemonDetail?.moves ?? [],
-            types: pokemonDetail?.types ?? [],
-          }}
-        />
-      </div>
+    
     </>
   )
 }
